bf79bc273eb930e7b1952080c48d16fec63fe2c0
Colin Kinloch <colin@kinlo.ch>
2017-06-16 13:35:45 +0100

    updated bindgen

--- gen.sh
+++ gen.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-bindgen /usr/include/lilv-0/lilv/lilv.h --link=dynamic=lilv-0 --builtins --output src/lilv.rs 
+bindgen /usr/local/include/lilv-0/lilv/lilv.h -llilv-0 --builtins --no-unstable-rust --output src/lilv.rs 
